173cf3eee6d5a5bf588198902e29a916b1e5a5a7
yangxuanhui <578986944@qq.com>
2019-03-08 00:40:57 +0800

    add comments

--- wordladder/src/main/java/Dict.java
+++ wordladder/src/main/java/Dict.java
@@ -2,9 +2,10 @@ import java.io.*;
 import java.util.Vector;
 
 public class Dict {
+    //字典类，包含所有合法单词
     Vector<String> dictionary = new Vector();
     public Dict(String path){
-
+        //读文件
         try{
             FileReader reader = new FileReader(path);
             BufferedReader br = new BufferedReader(reader);
@@ -14,9 +15,11 @@ public class Dict {
             }
             System.out.println(dictionary.capacity());
         } catch (IOException e){
+            //异常输出
             e.printStackTrace();
         }
     }
+    //检查单词是否在字典中
     boolean inDict(String word){
         return dictionary.contains(word);
     }
--- wordladder/src/main/java/Ladder.java
+++ wordladder/src/main/java/Ladder.java
@@ -1,31 +1,52 @@
 import java.io.*;
 import java.util.*;
+
 public class Ladder {
     Dict dictionary;
-    Stack<String> result;
-    String begin,end;
+    Stack<String> result; //保存最终结果
+    String begin,end; //wordladder开始和结尾的单词
+
+    //构造函数
     public Ladder(String begin, String end,Dict dict){
         this.begin = begin;
         this.end = end;
         this.dictionary = dict;
-        result = getLadder();
+        //result = getLadder();
     }
+
+    //得到 wordladder
     Stack<String> getLadder(){
+        //存储中间可能的ladder的队列
         Queue<Stack<String>> ladders = new LinkedList<Stack<String>>();
+        //存储可能的ladder
         Stack<String> part_ladder = new Stack();
+        //初始化
         part_ladder.push(begin);
         ladders.add(part_ladder);
+        //用来存每个可能ladder中的尾部(栈顶)的单词
         String last;
+        //储存某单词的所有相邻的单词
+        //相邻：只有一个字母不同，只多一个字母，只少一个字母
         TreeSet<String> neighbors = new TreeSet();
+
+        //不断从队首取出可能的ladder，在队尾加入更长的ladder
+        //直到队列为空或得到最终结果
         while(!ladders.isEmpty()){
+            //取出队首元素
             part_ladder = ((LinkedList<Stack<String>>) ladders).poll();
+            //取出栈顶元素
             last = part_ladder.peek();
+            //得到last的所有neighbor
             getNeighbors(last,neighbors);
+
+            //检查每一个neighbor
             for(String neighbor : neighbors){
+                //如果和结尾单词相同，得到结果，返回
                 if(neighbor == end){
                     part_ladder.push(neighbor);
                     return  part_ladder;
                 }
+                //如果和结尾单词不同，且和当前ladder中单词不重复，加入到ladder尾(栈顶)
                 else if(neighbor!=end && !part_ladder.contains(neighbor)){
                     Stack<String> new_ladder = part_ladder;
                     new_ladder.push(neighbor);
@@ -33,20 +54,24 @@ public class Ladder {
                 }
             }
         }
+        //没有结果，返回null
         return null;
     }
 
+    //得到某单词的所有neighbor，并存在neighbors中
     void getNeighbors(String word,TreeSet<String> neighbors){
         int size = word.length();
+        //修改该单词，增、删、改
         StringBuffer origin = new StringBuffer(word),ngb_replace, ngb_insert, ngb_delete;
         String n_replace, n_insert,n_delete;
         for(int i = 0; i < size; i++){
             for(int j = 0; j < 26; j++){
                 String letter = String.valueOf(97+j);
                 ngb_insert = origin.insert(i,(char)97+j);
-                ngb_replace = origin.replace(i,i+2,letter);
+                ngb_replace = origin.replace(i,i+2,letter);    //origin插入了一个字母，所以修改两个字母(i+2)
                 n_insert = ngb_insert.toString();
                 n_replace = ngb_replace.toString();
+                //检查是否是字典中的单词
                 if(dictionary.inDict(n_insert)){
                     neighbors.add(n_insert);
                 }
@@ -62,14 +87,30 @@ public class Ladder {
         }
     }
 
+    //输出结果
+    void print(){
+        if(result!=null){
+            System.out.println("WordLadder: ");
+            int size = result.size();
+            for(int i =0; i<size-1;i++){
+                System.out.println(result.elementAt(i) + " --> ");
+            }
+            System.out.println(result.elementAt(size-1));
+        }
+        else{
+            System.out.println("No Ladder from "+ begin + " to " + end);
+        }
+    }
     public static void main(String args[]) {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String path,begin,end;
             System.out.println("-------Word Ladder-------");
             System.out.println("Dictionary file path: ");
+            //根据输入路径创建字典对象
             path = br.readLine();
             Dict dict = new Dict(path);
+            //throw ?
             while(true) {
                 System.out.println("Word #1 (or Enter to quit): ");
                 begin = br.readLine();
@@ -78,7 +119,9 @@ public class Ladder {
                 //ignore case
                 // check word
                 Ladder result = new Ladder(begin,end,dict);
-                //print ladder
+                result.getLadder();
+                //输出结果
+                result.print();
             }
         } catch (IOException e) {
             e.printStackTrace();
